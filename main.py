487ae9413fb56d8a16ab3d63f4846fbff4b9fd1f
Mason Mapfundematsva <masonmapfunde@gmail.com>
2023-03-04 20:34:47 +0200

    save text method

--- main.py
+++ main.py
@@ -29,6 +29,30 @@ def reset_app():
     return
 
 
+def save_text():
+    global user_text
+    if user_text == "":
+        return
+    try:
+        f = open('writeups.txt', 'r')
+    except FileNotFoundError:
+        f = open('writeups.txt', 'w')
+        f.write(user_text)
+        user_text = ""
+        return
+    else:
+        cont = f.read()
+        if cont == "":
+            text_to_write = user_text
+        else:
+            text_to_write = f'\n{user_text}'
+
+        with open('writeups.txt', 'a') as f:
+            f.write(text_to_write)
+            user_text = ""
+    finally:
+        return
+
 
 # UI SETUP
 BORDER = "#3C2C3E"
